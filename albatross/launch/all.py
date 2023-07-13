a4800b9a89bb6d3f0b719ab7b442722a9a9758ce
Underwater Drones Project Team <inuwrobo@lehigh.edu>
2023-07-13 16:37:03 -0400

    Creating a launch file and linking the yolov8 model

---
+++ albatross/launch/all.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+def generate_launch_description():
+    yoloconfig = IncludeLaunchDescription(
+        # PythonLaunchDescriptionSource([os.path.join(
+        # get_package_share_directory('yolov8_bringup'), 'launch'),
+        # 'yolov8.launch.py'
+        # ])
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('yolov8_bringup'),
+                'launch',
+                'yolov8.launch.py'
+            ])
+        ])
+    )
+
+    return LaunchDescription([
+        Node(
+            package='realsense2_camera',
+            namespace='namespace',
+            executable='realsense2_camera_node',
+            name='camera'
+        ),
+        yoloconfig
+    ])
